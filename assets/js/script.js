dd19c650efa17a3c4a5dd55c85b3e6f07a3ecfc1
jack <jackrozovsky@gmail.com>
2024-07-09 16:30:02 -0400

    Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,34 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+const task = $("#task");
+const date = $("#date");
+const text = $("#text");
+const save = $("#save");
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    return String.fromCharCode(Math.floor(Math.random() * 77) + 34);
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-
+    const info = {
+        task: task.val.trim(),
+        date: date.val.trim(),
+        text: text.val.trim(),
+    };
+    localStorage.setItem('info', JSON.stringify(info))
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-
+    const taskList = JSON.parse(localStorage.getItem('info'));
+    if (taskList === null) {
+        $("task") = info.task,
+        $("date") = info.date,
+        $("text") = info.text
+    }
 }
 
 // Todo: create a function to handle adding a new task
@@ -34,5 +48,8 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+    event.isDefaultPrevented();
+    saveTask();
+    renderTaskList();
 
 });
